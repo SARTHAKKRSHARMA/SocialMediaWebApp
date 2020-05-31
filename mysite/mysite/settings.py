c63dba6ab13f648d0e55c3d52c8efa388b7d51b8
Sarthak <f20190449@pilani.bits-pilani.ac.in>
2020-06-01 00:20:21 +0530

    Social Media commit#3

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -23,7 +23,7 @@ MEDIA_DIR  = os.path.join(BASE_DIR, 'media')
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SOCIAL_MEDIA_SECRET_KEY')
-SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = os.environ.get('SOCIAL_AUTH_GOOGLE_OAUTH2')
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = os.environ.get('SOCIAL_AUTH_GOOGLE_OAUTH2_KEY')
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = os.environ.get('SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
